d3b2354352c42cf7ba26aebb88686720e5cdff50
VikingCyber <viking602ki557@mail.ru>
2025-03-17 23:25:34 +0700

    icon fix

--- app/src/main/java/com/viking/pathfinder/MapFragment.java
+++ app/src/main/java/com/viking/pathfinder/MapFragment.java
@@ -3,7 +3,6 @@ package com.viking.pathfinder;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
